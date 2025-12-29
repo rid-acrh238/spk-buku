8d02af71053293f3a15cb37accc34e3fda1e2e40
wildanridloal2803-bit <wildanridloal2803@gmail.com>
2025-12-29 11:44:32 +0700

    pliss jalan

--- config/database.js
+++ config/database.js
@@ -16,7 +16,7 @@ const db = mysql.createPool({
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'spk_pemilihan_buku',
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 5000,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
